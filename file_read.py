bed51bec50afda87e24dc3f307ea9d89fe417ef4
Lightman, Matthew <matthew.lightman@chase.com>
2014-10-12 14:26:10 -0400

    Added file_read.py

---
+++ file_read.py
@@ -0,0 +1,24 @@
+"""
+These are some methods for reading and
+parsing files.
+"""
+
+def manage_line(line) :
+    """
+    Splitting the line returns a list of strings that have been split
+    by some value (default white space), then print if the second value
+    is greater than the first
+    """
+    splitline = line.split()
+    if int(splitline[1]) > int(splitline[2]) :
+        print line
+        return line
+
+def funcread(fname) : 
+    # Create a file handle so I can read the file
+    f = open(fname,'r')
+    for line in f : 
+        # Here's where I do stuff...
+        manage_line(line)
+
+funcread('data1.txt')
